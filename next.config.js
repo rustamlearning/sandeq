09d9bb0dbd5865ea5af2c310d53e7d7681899276
Abid Rustam <rustammacbook@Rustams-MacBook-Air.local>
2026-04-25 08:11:57 +0800

    Add Cloudflare Pages configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
